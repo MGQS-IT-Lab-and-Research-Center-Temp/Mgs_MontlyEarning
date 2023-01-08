93d661938cdb5143fc59ade0b16afbadf5af1d69
AbdulWaheedMahmoodAhmad <107749621+opemiposi123@users.noreply.github.com>
2023-01-08 21:50:37 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MonthlyEarning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input no of hour(s) of work");
+                double hoursOfWork = Double.Parse(Console.ReadLine());
+
+              
+                if (hoursOfWork >= 15)
+                {
+                    hoursOfWork = 15;
+                }
+                else
+                {
+                    double hour = Salary.Earning(hoursOfWork);
+                    Console.WriteLine(hour);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+        }     
+    }
+}
---
+++ Salary.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyEarning
+{
+    public class Salary
+    {
+        public static double Earning(double hoursOfWork)
+        {
+            double earningPerDay = 25 * hoursOfWork;
+            double earningPerWeek = 5 * earningPerDay;
+            double earningPerMonth = 4 * earningPerWeek;
+            Console.WriteLine($"Gross money is = {earningPerMonth} dollar");
+
+            if (hoursOfWork > 8 && hoursOfWork <= 24)
+            {
+                double overTime = (hoursOfWork - 8) * 10;
+                double sum = overTime + earningPerDay;
+                Console.WriteLine($"over time money is {sum} dollar");
+            }
+
+            if (earningPerMonth <= 1000)
+            {
+                double minTax = ((double)3 / 200) * earningPerMonth;
+                earningPerMonth -= minTax;
+                Console.WriteLine($"net amount is{earningPerMonth} dollar");
+            
+            }
+
+            if (earningPerMonth > 1000 && earningPerMonth < 2500)
+            {
+                double mediumTax = ((double)5 / 200) * earningPerMonth;
+                earningPerMonth -= mediumTax;
+                Console.WriteLine($"Net amount is{earningPerMonth}dollar");
+            }
+
+            if (earningPerMonth > 2500)
+            {
+                double maxTax = ((double)9 / 200) * earningPerMonth;
+                earningPerMonth -= maxTax;
+                Console.WriteLine($"net earning is = {earningPerMonth}");
+            }
+            Console.Write($"amount of money per week dollar :$");
+            return earningPerWeek;
+
+
+
+        }
+
+    }
+}
