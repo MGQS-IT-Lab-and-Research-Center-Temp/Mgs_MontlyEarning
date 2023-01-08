a96b08907e3da0c62c55f7e7b759d899a870ba9e
AbdulWaheedMahmoodAhmad <107749621+opemiposi123@users.noreply.github.com>
2023-01-08 21:59:48 +0100

    change message

--- Salary.cs
+++ Salary.cs
@@ -12,7 +12,7 @@ namespace MonthlyEarning
             double earningPerDay = 25 * hoursOfWork;
             double earningPerWeek = 5 * earningPerDay;
             double earningPerMonth = 4 * earningPerWeek;
-            Console.WriteLine($"Gross money is = {earningPerMonth} dollar");
+            Console.WriteLine($"Gross money is = {earningPerMonth} dolar");
 
             if (hoursOfWork > 8 && hoursOfWork <= 24)
             {
